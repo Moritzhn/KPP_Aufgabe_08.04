606ae2002935897659274a818f7a32c9ea20ac2e
Moritzhn <119943162+Moritzhn@users.noreply.github.com>
2023-01-17 14:20:08 +0100

    Update 08.04.c

--- 08.04.c
+++ 08.04.c
@@ -6,7 +6,8 @@
     char string[] = "C:\\Eigene Dateien\\FOM\\C-Code\\main.c";
     char* Extension = strrchr(string, '.');
     printf("Extension: %s \n", Extension);
-  
+    char* Dateiname 0 strrchr(string, '\\');
+    printf("Dateiname: %s \n", Dateiname);
     
     return 0;
   }
