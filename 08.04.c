1e416ca49d9a52456f11ceaaa23be40ebda29f8b
Moritzhn <119943162+Moritzhn@users.noreply.github.com>
2023-01-17 14:16:21 +0100

    Create 08.04.c

---
+++ 08.04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+  int main()
+  {
+    char string[] = "C:\Eigene Dateien\FOM\C-Code\main.c";
+    char* Extension = strrchr(string, '.');
+    printf("Extension: %s \n", Extension);
+  
+    
+    return 0;
+  }
